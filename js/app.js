b59b97fa77747628bcd988bfe42fc1a21092cb0e
Youness RHANDOUR <rhandour.youness@gmail.com>
2016-01-22 17:58:56 +0000

    level 5 servce but didn't work for me.

--- js/app.js
+++ js/app.js
@@ -31,9 +31,15 @@
         }
     });
     
-    app.controller('StoreController',function(){
-        this.products = gems;
-    });
+    app.controller('StoreController',['$http',function($http){
+        var store = this;
+        
+        store.products = []; 
+        
+        $http.get('/data/products.json').success(function(data){
+            store.products = data;    
+        });
+    }]);
     
      app.controller('PanelController',function(){
         this.tab = 1;
@@ -58,115 +64,6 @@
 
     });
     
-    var gems = [
-        {
-            name:"Dodecahedron",
-            price:2.95,
-            description:"lorem  ipsum",
-            images:[
-                {
-                    full:"Hydrangeas.jpg",
-                    thumb:"Hydrangeas-thumb.jpg",
-                    
-                },
-                 {
-                    full:"Hydrangeas.jpg.jpg",
-                    thumb:"Hydrangeas-thumb.jpg",
-                    
-                },
-                 {
-                    full:"Hydrangeas.jpg",
-                    thumb:"Hydrangeas-thumb.jpg",
-                    
-                },
-            ],
-            reviews: [
-                { 
-                    stars: 5,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                },
-                { 
-                    stars: 1,
-                    body: "This product sucks",
-                    author: "tim@hater.com"
-                }
-            ],
-            canPurchase:true,
-            soldOut:false,
-        },
-        {
-            name:"pentagonal Gem",
-            price:2.95,
-            description:"lorem  ipsum",
-            images:[
-                {
-                    full:"Chrysanthemum.jpg",
-                    thumb:"Chrysanthemum-thumb.jpg",
-                    
-                },
-                {
-                    full:"Chrysanthemum.jpg",
-                    thumb:"Chrysanthemum-thumb.jpg",
-                    
-                },
-                {
-                    full:"Chrysanthemum.jpg",
-                    thumb:"Chrysanthemum-thumb.jpg",
-                    
-                }
-            ],
-            reviews: [
-                { 
-                    stars: 5,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                },
-                { 
-                    stars: 1,
-                    body: "This product sucks",
-                    author: "tim@hater.com"
-                }
-            ],
-            canPurchase:false,
-            soldOut:true,
-        },
-        {
-            name:"Dodecahedron",
-            price:2.95,
-            description:"lorem  ipsum lorem  ipsum",
-            images:[
-                {
-                    full:"Desert.jpg",
-                    thumb:"Desert-thumb.jpg",
-                    
-                },
-                 {
-                    full:"Desert.jpg.jpg",
-                    thumb:"Desert-thumb.jpg",
-                    
-                },
-                 {
-                    full:"Desert.jpg",
-                    thumb:"Desert-thumb.jpg",
-                    
-                },
-            ],
-            reviews: [
-                { 
-                    stars: 5,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                },
-                { 
-                    stars: 1,
-                    body: "This product sucks",
-                    author: "tim@hater.com"
-                }
-            ],
-            canPurchase:true,
-            soldOut:true,
-        }
-    ]
+    
     
 })();
